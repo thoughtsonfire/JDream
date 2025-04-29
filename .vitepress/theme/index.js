8a3f69e47beadb76398c5e5cc4c314e6d085223a
Jinmengxin <jinmengxinwork@163.com>
2025-04-29 15:51:15 +0800

    aaaa

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -10,7 +10,8 @@ import "element-plus/dist/index.css";
 import backtotop from "./component/backtotop.vue"
 import mediumZoom from 'medium-zoom';
 import { onMounted, watch, nextTick } from 'vue';
-import { useRoute } from 'vitepress';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
@@ -41,6 +42,28 @@ export default {
     const initZoom = () => {
       mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
     };
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+    
+        
+    // giscus配置
+    giscusTalk({
+      repo: 'thoughtsonfire/JDream', //仓库
+      repoId: 'R_kgDOMAsV9g', //仓库ID
+      category: 'General', // 讨论分类
+      categoryId: 'DIC_kwDOMAsV9s4CplMI', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    );
     onMounted(() => {
       initZoom();
     });
