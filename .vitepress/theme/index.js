50d43d7ecbb524da3d62eb5c8bce3e78c0d358f9
Jinmengxin <jinmengxinwork@163.com>
2025-04-29 14:44:03 +0800

    giscus-backup

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -45,27 +45,27 @@ export default {
     };
 
     // Get frontmatter and route
-    const { frontmatter } = useData();
+    // const { frontmatter } = useData();
     
         
     // giscus配置
-    giscusTalk({
-      repo: 'thoughtsonfire/JDream', //仓库
-      repoId: 'R_kgDOMAsV9g', //仓库ID
-      category: 'General', // 讨论分类
-      categoryId: 'DIC_kwDOMAsV9s4CplMI', //讨论分类ID
-      mapping: 'pathname',
-      inputPosition: 'bottom',
-      lang: 'zh-CN',
-      }, 
-      {
-        frontmatter, route
-      },
-      //默认值为true，表示已启用，此参数可以忽略；
-      //如果为false，则表示未启用
-      //您可以使用“comment:true”序言在页面上单独启用它
-      true
-    );
+    // giscusTalk({
+    //   repo: 'thoughtsonfire/JDream', //仓库
+    //   repoId: 'R_kgDOMAsV9g', //仓库ID
+    //   category: 'General', // 讨论分类
+    //   categoryId: 'DIC_kwDOMAsV9s4CplMI', //讨论分类ID
+    //   mapping: 'pathname',
+    //   inputPosition: 'bottom',
+    //   lang: 'zh-CN',
+    //   }, 
+    //   {
+    //     frontmatter, route
+    //   },
+    //   //默认值为true，表示已启用，此参数可以忽略；
+    //   //如果为false，则表示未启用
+    //   //您可以使用“comment:true”序言在页面上单独启用它
+    //   true
+    // );
     
     onMounted(() => {
       initZoom();
